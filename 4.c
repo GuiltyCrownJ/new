c46bed4a798e9683788244f191f594ec7f44aca6
Haonan Chen <97212304+GuiltyCrownJ@users.noreply.github.com>
2022-01-10 22:29:30 +0800

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+int Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+int main()
+{
+
+	//printf("\'\n");
+	//printf("%s\n","1564");
+	//printf("%c\n", '0');
+	//printf("\154\n");
+	//printf("\x30\n");
+	//printf("%d\n",strlen("\t\3288888"));
+	/*printf("是否要学习（1是，2否）:");
+	int input ;
+	scanf("%d", &input);
+	if (input == 1)
+		printf("好offer\n");
+	else
+		printf("寄");
+	return 0;
+
+	*/
+	/*int line=0;
+	while (line < 30000)
+	{
+		printf("写代码: %d\n", line);
+		line++;
+	}
+
+		if (line == 30000)
+		{
+			printf("good offer");
+	}*/
+
+	//int 	num1 = 1, num2 = 2,
+	//	//num = num1 + num2;
+	//	num = Add(num1, num2);
+
+	//	printf("%d\n%d\n%d", num1, num2,num);
+
+	//	 
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	////char arr2[5] = { 'a','b','c' };
+	//int i = 0;
+	//while (i < 10)
+	//{
+	//	printf("%d\t",arr[i]);
+	//	i++;
+	//}
+	//int a = -1;
+	//int arr[10];
+	//printf("%d\n", sizeof a);
+	//printf("%d\n", sizeof arr);
+	//printf("%d\n", sizeof arr[7]);
+	//printf("%d\n", sizeof arr/sizeof arr[0]);//num of elements
+	//
+	//int a = -1;                  // positive number stay constant.
+	//printf("%d\n", ~a);          // 100000000000000000000000000000000001(ture form）
+		                             // 111111111111111111111111111111111110(inverse code)
+	                               // 111111111111111111111111111111111111(two's complyment representation)
+	              //~a(a totally take the opposite):
+                                 // 000000000000000000000000000000000000	
+	/*int a = 10,
+		b = a++, c,
+	    d = --a;
+		c = ++a;
+
+	printf("%d %d %d  ",d, b, c);*/
+	
+	
+	//int a = (int)3.14;
+	//printf("%d\n", a);
+	
+	
+	
+	int a = 3, b = 0, c = a && b, d = a || b,
+		e = a > b ? a : b,
+		f = (a = b + a, c = 385, b = 33)
+		;
+	//[]    下标引用操作符   （）函数调用操作符
+	
+	printf("%d\n", c); //385
+		
+	printf("%d\n", d);//1
+	printf("%d\n", e);//3
+	printf("%d\n", f);//33
+
+    
+	return 0;
+
+	
+
+}
